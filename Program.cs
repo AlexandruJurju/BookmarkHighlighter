abb569ffedf37374f6fe270f427ba02de20da8b1
Alexandru.I.Jurju <alexandru.i.jurju@gmail.com>
2023-12-31 10:56:56 +0200

    changed path of bookmark folder to local install

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ class Program
 {
     static void Main()
     {
-        string bookmarksFilePath = @"C:\Users\realb\AppData\Local\Vivaldi\User Data\Default\Bookmarks";
+        string bookmarksFilePath = @"X:\Misc Programs\Browser\Vivaldi\User Data\Default\Bookmarks";
         GamesParser parser = new GamesParser();
         Dictionary<String, List<String>> games = parser.GetLinks(bookmarksFilePath);
         Highlighter.WriteGameJsFiles(games);
