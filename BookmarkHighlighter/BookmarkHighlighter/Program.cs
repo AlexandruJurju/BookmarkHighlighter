62e322a97354295b219c60c843f04bff0ae3babf
Alex <alexandru.i.jurju@gmail.com>
2024-09-10 06:39:56 +0300

    nexus mods - color undownloaded mods with red

--- BookmarkHighlighter/BookmarkHighlighter/Program.cs
+++ BookmarkHighlighter/BookmarkHighlighter/Program.cs
@@ -5,8 +5,8 @@ using BookmarkHighlighter.JsWriters.NexusModsWriter;
 
 // todo: make it so js file is only written once, first find all the links then write
 
-var bookmarksFilePath = @"/run/media/alex/storage/Browser Downloads/bookmarks-2024-07-28.json";
-IBookmarkParser parser = new FirefoxBackupParser(bookmarksFilePath);
+var bookmarksFilePath = @"C:\Users\realb\AppData\Local\Vivaldi\User Data\Default\Bookmarks";
+IBookmarkParser parser = new VivaldiBookmarksParser(bookmarksFilePath);
 var bookmarkStructure = parser.GetBookmarkStructure();
 
 var outputPath = @"Highlighter Extension/GameHighlighter.js";
@@ -23,3 +23,7 @@ gamesJsWriter.Write(games);
 var nexusMods = nexusModsParser.Parse(bookmarkStructure);
 var nexusModsJsWriter = new NexusModsJsWriter(outputPath);
 nexusModsJsWriter.Write(nexusMods);
+
+Console.WriteLine("DONE!");
+Console.WriteLine("PRESS ANY KEY TO EXIT");
+Console.ReadKey();
